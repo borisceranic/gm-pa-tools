00f48d77a15b7121f748ef2be487a3785ca87b3a
Boris Ceranic <me@sosko.in.rs>
2014-10-18 09:20:59 +0200

    Modify scans.js to make URL configuration easier

--- app/scans.js
+++ app/scans.js
@@ -1,13 +1,19 @@
 GM_define(
   ['lib/post', 'lib/cdata'],
   function(post, cdata) {
+
+    // URL where HTTP POST will be sent
+    var submitURL = "https://sosko.in.rs/pa/";
+    // parameters to send in body ([SCAN] is replaced with scan data)
+    var bodyParams = "parse=1&parsing=[SCAN]";
+
     var id = function(id) { return document.getElementById(id); };
     return function(document) {
       var scan = id('scan');
       if (scan) {
         console.info("GM SCANS: #scan found, sending data...");
         var text = encodeURIComponent(cdata(scan));
-        post("https://sosko.in.rs/pa/", "parse=1&parsing="+text, function(response){
+        post(submitURL, bodyParams.replace('[SCAN]', text), function(response){
           console.log("GM SCANS: sending complete, response received, status: "+response.status);
         });
       }
